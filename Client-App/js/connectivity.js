c998093ad566a50a7454d3012f8cd9dc3be62be1
Jerom Dreux <jd@izorian.co.uk>
2016-11-13 09:35:23 +0000

    Dev Migration

---
+++ Client-App/js/connectivity.js
@@ -0,0 +1,39 @@
+var loaded;
+
+$(function() {
+
+  displayIPAddresses();
+
+  $(document).bind("keypress", "o", function () {
+		gBackgroundPage.showOptions().bind(gBackgroundPage);
+  });
+  
+});
+
+function displayIPAddresses() {
+  
+  $("body").empty();
+    
+  netInterfaces().then(function(interfaces) {
+    var container = $("<div />", {class : "centered"});
+    $.each(interfaces, function(i, interface) {
+      $("<div />", {
+        class : "address"
+      }).append(
+        $("<h1 />")
+        .append(
+          $("<span />", {class : "lighter", text : interface.name + " "})
+        ).append(
+          $("<span />", {class : "darker", text : interface.address})
+        ).append(
+          $("<span />", {class : "lighter", text : "/" + interface.prefixLength})
+        )).appendTo(container);
+    })
+    container.appendTo("body");
+
+    if (gBackgroundPage.timeout)
+      setTimeout(gBackgroundPage.reTry.bind(gBackgroundPage), gBackgroundPage.timeout * 1000);
+
+  });
+  
+}
